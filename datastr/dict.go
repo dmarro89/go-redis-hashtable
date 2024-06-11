b16ab22a88df8b173b7af574176d7ba2f94fda52
go-while <go-while@users.noreply.github.com>
2024-06-11 15:50:04 +0200

    already declared

--- datastr/dict.go
+++ datastr/dict.go
@@ -362,63 +362,6 @@ func (d *Dict) delete(key string, dolock bool) *DictEntry {
 	return nil
 }
 
-// Get returns the value associated with the given key in the dictionary.
-//
-// Parameters:
-// - key: the key to look up in the dictionary.
-//
-// Return:
-// - interface{}: the value associated with the key, or nil if the key is not found.
-func (d *Dict) Get(key string) interface{} {
-	d.rwmux.RLock()
-	defer d.rwmux.RUnlock()
-	entry := d.getEntry(key)
-	if entry == nil {
-		return nil
-	}
-	retval := entry.value
-	return retval // copy avoids race conditions
-}
-
-// Set sets the value of a key in the dictionary.
-//
-// Parameters:
-//   - key: the key to set the value for.
-//   - value: the value to set.
-//
-// Returns:
-//   - error: an error if the key already exists in the dictionary.
-func (d *Dict) Set(key string, value interface{}) error {
-	d.rwmux.Lock()
-	defer d.rwmux.Unlock()
-	entry := d.getEntry(key)
-	if entry != nil {
-		entry.value = value
-		return nil
-	}
-	retval := d.add(key, value) // copy avoids race conditions
-	return retval
-}
-
-// Delete deletes an entry from the dictionary.
-//
-// Parameters:
-// - key: the key of the entry to be deleted.
-//
-// Returns:
-// - error: if the entry is not found.
-func (d *Dict) Delete(key string) error {
-	d.rwmux.Lock()
-	defer d.rwmux.Unlock()
-	dictEntry := d.delete(key, false)
-	if dictEntry == nil {
-		return fmt.Errorf(`entry not found`)
-	}
-	d.logger.Debug("deleted key='%s'", key)
-	return nil
-}
-
-
 // GenerateRandomBytes generates a fixed slice of 16 random bytes
 //
 // Parameters:
--- datastr/operations.go
+++ datastr/operations.go
@@ -10,11 +10,14 @@ import "fmt"
 // Return:
 // - interface{}: the value associated with the key, or nil if the key is not found.
 func (d *Dict) Get(key string) interface{} {
+	d.rwmux.RLock()
+	defer d.rwmux.RUnlock()
 	entry := d.getEntry(key)
 	if entry == nil {
 		return nil
 	}
-	return entry.value
+	retval := entry.value
+	return retval // copy avoids race conditions
 }
 
 // Set sets the value of a key in the dictionary.
@@ -26,12 +29,15 @@ func (d *Dict) Get(key string) interface{} {
 // Returns:
 //   - error: an error if the key already exists in the dictionary.
 func (d *Dict) Set(key string, value interface{}) error {
+	d.rwmux.Lock()
+	defer d.rwmux.Unlock()
 	entry := d.getEntry(key)
 	if entry != nil {
 		entry.value = value
 		return nil
 	}
-	return d.add(key, value)
+	retval := d.add(key, value) // copy avoids race conditions
+	return retval
 }
 
 // Delete deletes an entry from the dictionary.
@@ -42,9 +48,13 @@ func (d *Dict) Set(key string, value interface{}) error {
 // Returns:
 // - error: if the entry is not found.
 func (d *Dict) Delete(key string) error {
-	dictEntry := d.delete(key)
+	d.rwmux.Lock()
+	defer d.rwmux.Unlock()
+	dictEntry := d.delete(key, false)
 	if dictEntry == nil {
 		return fmt.Errorf(`entry not found`)
 	}
+	d.logger.Debug("deleted key='%s'", key)
 	return nil
 }
+
